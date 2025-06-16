58db603c479357e3904f21c4151d77d51743293e
DerefNullPointer <databasesandpointers@gmail.com>
2025-06-16 08:37:11 -0500

    Initialize Spring Boot project with Thymeleaf, JPA, and MySQL

---
+++ src/main/java/com/github/derefnullpointer/mealmixup/MealMixupApplication.java
@@ -0,0 +1,13 @@
+package com.github.derefnullpointer.mealmixup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMixupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMixupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/derefnullpointer/mealmixup/MealMixupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.derefnullpointer.mealmixup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMixupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
