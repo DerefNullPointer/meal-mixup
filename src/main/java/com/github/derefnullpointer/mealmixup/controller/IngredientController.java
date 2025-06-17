e8289e435adf8757ec06c4b6d6dd132a8c0beb89
DerefNullPointer <databasesandpointers@gmail.com>
2025-06-16 23:10:50 -0500

    change logic for just listing ingredients to more general form to account for new CRUD operations (create, delete)

--- src/main/java/com/github/derefnullpointer/mealmixup/controller/IngredientController.java
+++ src/main/java/com/github/derefnullpointer/mealmixup/controller/IngredientController.java
@@ -1,11 +1,14 @@
 package com.github.derefnullpointer.mealmixup.controller;
 
+import com.github.derefnullpointer.mealmixup.entity.Ingredient;
 import com.github.derefnullpointer.mealmixup.service.IngredientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/ingredients")
 public class IngredientController {
 
     private final IngredientService ingredientService;
@@ -14,9 +17,10 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/ingredients")
-    public String listIngredients(Model model) {
+    @GetMapping
+    public String showIngredientForm(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
         model.addAttribute("ingredients", ingredientService.getAllIngredients());
-        return "ingredients/list";
+        return "ingredients";
     }
 }
