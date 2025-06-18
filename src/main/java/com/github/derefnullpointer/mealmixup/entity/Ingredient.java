6a57a9095ad1236fc719e175b2d8a0931d3ab5b6
DerefNullPointer <databasesandpointers@gmail.com>
2025-06-17 20:29:13 -0500

    Refactor Ingredient entity to support meal association

--- src/main/java/com/github/derefnullpointer/mealmixup/entity/Ingredient.java
+++ src/main/java/com/github/derefnullpointer/mealmixup/entity/Ingredient.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ingredient")
@@ -21,8 +23,14 @@ public class Ingredient {
     @Size(max = 100, message = "Ingredient cannot be more than 100 characters.")
     private String name;
 
+    @Column(name = "created_by")
+    private String createdBy;
+
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MealIngredient> mealIngredients = new ArrayList<>();
 
     @PrePersist
     protected void onCreate() {
@@ -32,8 +40,9 @@ public class Ingredient {
     // Constructors
     public Ingredient() {}
 
-    public Ingredient(String name) {
+    public Ingredient(String name, String createdBy) {
         this.name = name;
+        this.createdBy = createdBy;
     }
 
     // Getters and setters
@@ -46,6 +55,19 @@ public class Ingredient {
     public void setName(String name) {
         this.name = name;
     }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+    public List<MealIngredient> getMealIngredients() {
+        return mealIngredients;
+    }
+    public void setMealIngredients(List<MealIngredient> mealIngredients) {
+        this.mealIngredients = mealIngredients;
+    }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
@@ -56,7 +78,9 @@ public class Ingredient {
         return "Ingredient{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", createdBy='" + createdBy + '\'' +
                 ", createdAt=" + createdAt +
+                ", mealIngredients=" + mealIngredients +
                 '}';
     }
 }
